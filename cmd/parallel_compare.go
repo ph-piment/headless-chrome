e01a13c1838b9dd86691faf6d4941624bcb84310
ph piment <ph.piment@gmail.com>
2020-03-25 20:28:55 +0900

    [add]add parallel_compare.go command

---
+++ cmd/parallel_compare.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+
+	"work/internal/browser"
+	"work/internal/image"
+)
+
+const compareOutputDir = "/go/src/work/outputs/images/compare/"
+
+func main() {
+	sourceURL := "https://www.google.com"
+	targetURL := "https://www.google.com"
+
+	ctx, allocCxl, ctxCxl := browser.GetContext()
+	defer allocCxl()
+	defer ctxCxl()
+
+	srcScshoByte, err := browser.GetFullScreenshotByteByURL(ctx, sourceURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tgtScshoByte, err := browser.GetFullScreenshotByteByURL(ctx, targetURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	srcImagePath := compareOutputDir + "source/image.png"
+	tgtImagePath := compareOutputDir + "target/image.png"
+	if err := image.WriteImageByByte(srcScshoByte, srcImagePath); err != nil {
+		log.Fatal(err)
+	}
+	if err := image.WriteImageByByte(tgtScshoByte, tgtImagePath); err != nil {
+		log.Fatal(err)
+	}
+	srcImage, err := image.ReadImageByPath(srcImagePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tgtImage, err := image.ReadImageByPath(tgtImagePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resImagePath := compareOutputDir + "result/image.png"
+	if err := image.CompareImage(srcImage, tgtImage, resImagePath); err != nil {
+		log.Fatal(err)
+	}
+}
